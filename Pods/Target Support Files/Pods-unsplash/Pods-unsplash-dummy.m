b535bb5350fd529c5ed4e9325580b2642874a069
Douglas Jacoveto <jdouglas@ciandt.com>
2021-06-06 18:23:28 -0300

    first commit

---
+++ Pods/Target Support Files/Pods-unsplash/Pods-unsplash-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_unsplash : NSObject
+@end
+@implementation PodsDummy_Pods_unsplash
+@end
--- unsplash/ViewController.swift
+++ unsplash/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageLoad: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func didLoadImage(_ sender: Any) {
+        
+        let url = URL(string: "https://images.unsplash.com/photo-1622960748104-094380e1d892?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMzc0NTR8MHwxfGFsbHx8fHx8fHx8fDE2MjI5ODU1MDI&ixlib=rb-1.2.1&q=80&w=1080")
+        
+        let processor = DownsamplingImageProcessor(size: imageLoad.bounds.size)
+                     |> RoundCornerImageProcessor(cornerRadius: 20)
+        
+        imageLoad.contentMode = .scaleAspectFill
 
+        imageLoad.kf.setImage(with: url, options: [
+            .processor(processor),
+            .scaleFactor(UIScreen.main.scale),
+            .transition(.fade(1)),
+            .cacheOriginalImage
+        ])
+    }
+    
 }
 
